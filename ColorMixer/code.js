12c9c410c48bd40e09f0a5ff2030e5e903eef7ed
abez117 <honestabez117@gmail.com>
2025-10-13 15:19:10 -0500

    Add files via upload

---
+++ ColorMixer/code.js
@@ -0,0 +1,11 @@
+function colorMixer()
+{
+    var red = document.getElementById("red").value;
+    var green = document.getElementById("green").value;
+    var blue = document.getElementById("blue").value;
+
+    document.getElementById("sample").style = "background-color: rgb("+ red + "," + green + "," + blue +");"
+    document.getElementById("output").innertHTML = "background-color: rgb("+ red + "," + green + "," + blue +");"
+
+
+}
