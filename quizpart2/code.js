437b5fae9ef72ea76b0fc746f5e4affcdf601b3a
abez117 <honestabez117@gmail.com>
2025-10-19 22:05:02 -0500

    Add files via upload

---
+++ quizpart2/code.js
@@ -0,0 +1,8 @@
+    function checkAnswers() {
+        let score = 0;
+        const totalQuestions = 5;
+
+      
+        const resultDiv = document.getElementById('result');
+        const percentage = (score / totalQuestions) * 100;
+    }
